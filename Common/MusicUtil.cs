19e69f901f7a70e34630f3b951710a4aa6421467
Leon Schimmel <leonschimmel15@gmail.com>
2023-02-12 23:59:25 +0100

    Moby

---
+++ Common/MusicUtil.cs
@@ -0,0 +1,9 @@
+﻿namespace Moby.Common;
+
+using Discord;
+
+public static class MusicUtil
+{
+    public static bool IsInvalidVoiceState(IVoiceState? voiceState)
+        => voiceState?.VoiceChannel is null || voiceState.IsDeafened || voiceState.IsSelfDeafened;
+}
--- Modules/MusicModule.cs
+++ Modules/MusicModule.cs
@@ -1,9 +1,12 @@
 ﻿namespace Moby.Modules;
 
 using Common;
+using Discord;
+using Discord.Interactions;
 using Services;
 using Victoria.Node;
 using Victoria.Player;
+using Victoria.Responses.Search;
 
 [Discord.Commands.Name("Music")]
 public sealed class MusicModule : MobyModuleBase
@@ -15,5 +18,17 @@ public sealed class MusicModule : MobyModuleBase
         _lava = lava;
     }
 
+    [SlashCommand("youtube", "Play anything from YouTube")]
+    public async Task YouTubeAsync([Summary("query", "A link from YouTube or a search term")] [MinLength(1)] [MaxLength(250)] string query)
+    {
+        await DeferAsync();
 
+        if (MusicUtil.IsInvalidVoiceState((IVoiceState)Context.User))
+        {
+            await FollowupAsync("You have to be in a voice channel and not deafened");
+            return;
+        }
+
+        
+    }
 }
